e48ddc12422d09f4d79c7c41282e9a76465de428
Alan Guillermo Avila Macias <g20160094@gmail.com>
2021-08-31 02:00:53 -0500

    [UPDATE] Forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :new_session
 
-  def login; end
+  def login
+    @user = User.new
+  end
 
   def new_session
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(user_params[:email])
     if @user.nil?
       redirect_to login_path, notice: 'The user does not exist'
-    elsif @user.valid_password?(params[:password])
+    elsif @user.valid_password?(user_params[:password])
       sign_in(:user, @user)
       redirect_to :root, notice: 'Session successfully started'
     else
@@ -18,5 +20,11 @@ class UsersController < ApplicationController
   def logout
     sign_out
     redirect_to :root, notice: 'You have successfully closed the session'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
