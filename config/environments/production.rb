6cc51a913ffe4d3989e9995c504b5a71b4f2d93c
Alan Guillermo Avila Macias <g20160094@gmail.com>
2021-09-06 15:15:38 -0500

    [UPDATE] Config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "frikr_production"
-Rails.application.routes.default_url_options[:host] = 'frikr-course.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'frikr-course.herokuapp.com', port: 587 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
